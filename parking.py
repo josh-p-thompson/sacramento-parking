e287addfd8590ce692e3e99638b22104bee42837
josh-p-thompson <jpthompson93@gmail.com>
2019-08-18 17:22:23 -0700

    consolidated functions

--- parking.py
+++ parking.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import requests
 import datetime
+import time
 from math import cos, asin, sqrt
+import numpy as np
 
-# creates df of parking data
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
 def get_parking_data():
     response = requests.get('https://opendata.arcgis.com/datasets/0060469c57864becb76a036d23236143_0.geojson').json()
     df = pd.io.json.json_normalize(response['features'])
@@ -11,9 +16,15 @@ def get_parking_data():
     new_columns = [col.rsplit('.')[1].upper() for col in df.columns.tolist()]
     df.columns = new_columns
     df[['LAT','LNG']] = pd.DataFrame(df['COORDINATES'].values.tolist(), index=df.index)
+    df['TIMELIMIT_VIOLATION'] = df['TIMELIMIT'].apply(timelimit_violation)
+    df['PKGTYPE_VIOLATION'] = df['PKGTYPE'].apply(pkgtype_violation)
+    df['NEXT_SWEEPING_PERIOD'] = df.apply(lambda x: next_sweeping_period(x['PKGSDAY'], x['PKGSWBEG'], x['PKGSWEND']), axis=1)
+    df['NEXT_SWEEPING_START'] = df['NEXT_SWEEPING_PERIOD'].apply(lambda x: None if x is None else x[0])
+    df['NEXT_SWEEPING_END'] = df['NEXT_SWEEPING_PERIOD'].apply(lambda x: None if x is None else x[1])
+    df['NEXT_SWEEPING_HOURS'] = df['NEXT_SWEEPING_START'].apply(time_to_sweeping)
     return df
 
-# gets distance between two points; source - https://stackoverflow.com/questions/41336756/find-the-closest-latitude-and-longitude
+# gets distance between two points, from -> https://stackoverflow.com/questions/41336756/find-the-closest-latitude-and-longitude
 def distance(lat1, lng1, lat2, lng2):
     p = 0.017453292519943295
     a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p)*cos(lat2*p) * (1-cos((lng2-lng1)*p)) / 2
@@ -23,7 +34,7 @@ def distance(lat1, lng1, lat2, lng2):
 def closest(coords, v):
     return min(coords, key=lambda p: distance(v[0],v[1],p[0],p[1]))
 
-# returns date of next street sweeping
+# returns date of next street sweeping datetime
 def next_sweeping_date(clean_date):
     if clean_date is None:
         return None
@@ -39,8 +50,8 @@ def next_sweeping_date(clean_date):
     return next_month.replace(day=(1 + (wkdy - next_month.weekday()) % 7))
 
 # returns start and end datetimes of next street cleaning
-def next_sweeping_period(clean_date, begin, end):
-    clean_date = next_sweeping_date(clean_date)
+def next_sweeping_period(clean_day, begin, end):
+    clean_date = next_sweeping_date(clean_day)
     if None in [clean_date, begin, end]:
         return None
     start_datetime = datetime.datetime.combine(clean_date, datetime.datetime.strptime(begin, '%I %p').time())
@@ -66,7 +77,7 @@ def timelimit_violation(limit):
 
 # returns True for parking type violations
 def pkgtype_violation(pkgtype):
-    if pkgtype in ['Red Zone', 'Single Space Meter', 'Red Zone (Fire Hydrant)', 'Alley', 'White Zone',
+    if pkgtype in ['Red Zone', 'Red Zone (Fire Hydrant)', 'Alley', 'White Zone',
                    'No Parking Any Time', 'Yellow Zone', 'Fire Hydrant', 'Crosswalk', 'Residential Zone',
                    'Single Space Meter (Double Space)', 'Crosswalk (No Markings)', 'No Parking Passenger Loading',
                    'Motorcycle Parking', 'R X R', 'Blue Zone', 'Cross Hatching', 'Taxi Zone', 'Bus Only',
@@ -74,35 +85,16 @@ def pkgtype_violation(pkgtype):
         return True
     return False
 
-# returns a dictionary for the coordinate
-def get_parking(coord):
-    coord_dict = df[(df['LAT'] == coord[0]) & (df['LNG'] == coord[1])].to_dict(orient='records')[0]
-    coord_dict['PARKING_VIOLATION'] = timelimit_violation(coord_dict['TIMELIMIT']) or pkgtype_violation(coord_dict['PKGTYPE'])
-    next_sweep = next_sweeping_period(coord_dict['PKGSDAY'], coord_dict['PKGSWBEG'], coord_dict['PKGSWEND'])
-    if next_sweep is None:
-        for key in ['NEXT_SWEEPING_START', 'NEXT_SWEEPING_END', 'NEXT_SWEEPING_HRS']:
-            coord_dict[key] = None
-    else:
-        coord_dict['NEXT_SWEEPING_START'] = next_sweep[0].strftime("%Y-%m-%d %H:%M:%S")
-        coord_dict['NEXT_SWEEPING_END'] = next_sweep[1].strftime("%Y-%m-%d %H:%M:%S")
-        coord_dict['NEXT_SWEEPING_HRS'] = time_to_sweeping(next_sweep[0])
-    return coord_dict
-
 class ParkingSpot:
     def __init__(self, coord):
         self.originalCoord = coord
         self.coordinates = closest(df['COORDINATES'].tolist(), coord)
-        self.all = get_parking(self.coordinates)
+        self.all = df[(df['LAT'] == self.coordinates[0]) & (df['LNG'] == self.coordinates[1])].to_dict(orient='records')[0]
         self.distMeters = distance(coord[0], coord[1], self.coordinates[0], self.coordinates[1]) * 1000
         self.fullAddress = f"{self.all['ADDRESS']} {self.all['STREET']}{self.all['SUFFIX']} {self.all['PREFIX']}"
         self.timeLimit = self.all['TIMELIMIT']
         self.parkingType = self.all['PKGTYPE']
         self.permitArea = self.all['PERMITAREA']
         self.eventArea = self.all['EVTAREA']
-        self.violation = self.all['PARKING_VIOLATION']
-        self.sweepStart = self.all['NEXT_SWEEPING_START']
-        self.sweepEnd = self.all['NEXT_SWEEPING_END']
-        self.sweepHrs = self.all['NEXT_SWEEPING_HRS']
-
-current = ParkingSpot([-121.50350012231698, 38.57408831516617])
-print(current.coordinates)
+        self.timeViolation = self.all['TIMELIMIT_VIOLATION']
+        self.parkingViolation = self.all['PKGTYPE_VIOLATION']
